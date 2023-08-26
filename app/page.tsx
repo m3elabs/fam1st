22237c466d516acaef90945b180b4b9232d7e076
m3 <m3.elabs@gmail.com>
2023-08-26 15:57:20 -0400

    test env

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,36 @@
-export default function Home() {
-  return (
-    <main className="flex justify-center items-center">
-      <img src={process.env.SOURCE}/>   
-    </main>
-  )
-}
+import React, { useEffect, useState } from 'react';
+
+  async function fetchImage() {
+    try {
+      const response = await fetch(process.env.SOURCE!);
+      const blob = await response.blob();  // Get the image data as a Blob
+      return blob;
+    } catch (error) {
+      console.error('Error fetching image:', error);
+      return null;
+    }
+  }
+  function ImageComponent() {
+    const [imageData, setImageData] = useState<Blob | null>(null);
+  
+    useEffect(() => {
+      async function loadImage() {
+        const blob = await fetchImage();
+        setImageData(blob);
+      }
+  
+      loadImage();
+    }, []);
+  
+    return (
+      <div>
+        {imageData && (
+          <img
+            src={URL.createObjectURL(imageData)}  // Create a temporary URL for the Blob
+            alt="Smile"
+          />
+        )}
+      </div>
+    );
+  }
+  export default ImageComponent
